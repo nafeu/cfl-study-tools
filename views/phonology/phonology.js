81d69f41fe17ed0903a1024b40d2de106375f9b7
Nafeu Nasir <nafeu.nasir@gmail.com>
2018-01-24 11:51:24 -0500

    Add remaining consonants

--- views/phonology/phonology.js
+++ views/phonology/phonology.js
@@ -156,6 +156,232 @@ angular.module('myApp.phonology', ['ngRoute'])
       ],
     },
 
+    {
+      name: "θ",
+      tags: [
+        "+cons",
+        "-son",
+        "+cont",
+        "-nas",
+        "-strident",
+        "-lat",
+        "-voice",
+        "-spread",
+        "Coronal",
+        "+ant",
+        "+distr",
+      ],
+    },
+
+    {
+      name: "s",
+      tags: [
+        "+cons",
+        "-son",
+        "+cont",
+        "-nas",
+        "+strident",
+        "-lat",
+        "-voice",
+        "-spread",
+        "Coronal",
+        "+ant",
+        "-distr",
+      ],
+    },
+
+    {
+      name: "ʃ",
+      tags: [
+        "+cons",
+        "-son",
+        "+cont",
+        "-nas",
+        "+strident",
+        "-lat",
+        "-voice",
+        "-spread",
+        "Coronal",
+        "-ant",
+        "+distr",
+      ],
+    },
+
+    {
+      name: "ç",
+      tags: [
+        "+cons",
+        "-son",
+        "+cont",
+        "-nas",
+        "-strident",
+        "-lat",
+        "-voice",
+        "-spread",
+        "Dorsal",
+        "+high",
+        "-back",
+      ],
+    },
+
+    {
+      name: "χ",
+      tags: [
+        "+cons",
+        "-son",
+        "+cont",
+        "-nas",
+        "+strident",
+        "-lat",
+        "-voice",
+        "-spread",
+        "Dorsal",
+        "-high",
+        "+back",
+      ],
+    },
+
+    {
+      name: "v",
+      tags: [
+        "+cons",
+        "-son",
+        "+cont",
+        "-nas",
+        "+strident",
+        "-lat",
+        "+voice",
+        "-spread",
+        "Labial",
+      ],
+    },
+
+    {
+      name: "ɣ",
+      tags: [
+        "+cons",
+        "-son",
+        "+cont",
+        "-nas",
+        "-strident",
+        "-lat",
+        "+voice",
+        "-spread",
+        "Dorsal",
+        "+high",
+        "+back",
+      ],
+    },
+
+    {
+      name: "m",
+      tags: [
+        "+cons",
+        "+son",
+        "-cont",
+        "+nas",
+        "-lat",
+        "+voice",
+        "-spread",
+        "Labial",
+      ],
+    },
+
+    {
+      name: "ɳ",
+      tags: [
+        "+cons",
+        "+son",
+        "-cont",
+        "+nas",
+        "-lat",
+        "+voice",
+        "-spread",
+        "Coronal",
+        "-ant",
+        "-distr",
+      ],
+    },
+
+    {
+      name: "ŋ",
+      tags: [
+        "+cons",
+        "+son",
+        "-cont",
+        "+nas",
+        "-lat",
+        "+voice",
+        "-spread",
+        "Dorsal",
+        "+high",
+        "+back",
+      ],
+    },
+
+    {
+      name: "r",
+      tags: [
+        "+cons",
+        "+son",
+        "+cont",
+        "-nas",
+        "-lat",
+        "+voice",
+        "-spread",
+        "+Coronal",
+        "+ant",
+        "-distr",
+      ],
+    },
+
+    {
+      name: "l",
+      tags: [
+        "+cons",
+        "+son",
+        "?cont",
+        "-nas",
+        "+lat",
+        "+voice",
+        "-spread",
+        "Coronal",
+        "+ant",
+        "-distr",
+      ],
+    },
+
+    {
+      name: "w",
+      tags: [
+        "-cons",
+        "+son",
+        "+cont",
+        "-nas",
+        "-lat",
+        "+voice",
+        "-spread",
+        "Labial",
+        "Dorsal",
+        "+high",
+        "+back",
+      ],
+    },
+
+    {
+      name: "h",
+      tags: [
+        "-cons",
+        "-son",
+        "+cont",
+        "-nas",
+        "-strident",
+        "-lat",
+        "-voice",
+        "+spread",
+      ],
+    },
+
     // {
     //   name: "",
     //   tags: [
