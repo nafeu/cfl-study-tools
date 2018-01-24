63059241fa09e1430415c4bbbeb04ce271bfa84c
Nafeu Nasir <nafeu.nasir@gmail.com>
2018-01-23 20:41:20 -0500

    Added feature explorer and first set of consonants

--- views/phonology/phonology.js
+++ views/phonology/phonology.js
@@ -13,4 +13,177 @@ angular.module('myApp.phonology', ['ngRoute'])
 
   $scope.title = "Phonology";
 
+  $scope.tag = function(message) {
+    if ($scope.tags) {
+      return $scope.tags.replace(/\s*,\s*/g, ',').split(',').every(function(tag) {
+        return message.tags.some(function(objTag){
+          return objTag.indexOf(tag) !== -1;
+        });
+      });
+    }
+    else {
+      return true;
+    }
+  };
+
+  $scope.data = [
+
+    {
+      name: "p",
+      tags: [
+        "+cons",
+        "-son",
+        "-cont",
+        "-nas",
+        "-strident",
+        "-lat",
+        "-voice",
+        "-spread",
+        "labial",
+      ],
+    },
+
+    {
+      name: "t",
+      tags: [
+        "+cons",
+        "-son",
+        "-cont",
+        "-nas",
+        "-strident",
+        "-lat",
+        "-voice",
+        "-spread",
+        "coronal",
+        "+ant",
+        "-distr",
+      ],
+    },
+
+    {
+      name: "t̪",
+      tags: [
+        "+cons",
+        "-son",
+        "-cont",
+        "-nas",
+        "-strident",
+        "-lat",
+        "-voice",
+        "-spread",
+        "coronal",
+        "+ant",
+        "+distr",
+      ],
+    },
+
+    {
+      name: "pf",
+      tags: [
+        "+cons",
+        "-son",
+        "-cont",
+        "-nas",
+        "+strident",
+        "-lat",
+        "-voice",
+        "-spread",
+        "labial",
+      ],
+    },
+
+    {
+      name: "tʃ",
+      tags: [
+        "+cons",
+        "-son",
+        "-cont",
+        "-nas",
+        "+strident",
+        "-lat",
+        "-voice",
+        "-spread",
+        "coronal",
+        "-ant",
+        "+distr",
+      ],
+    },
+
+    {
+      name: "kʰ",
+      tags: [
+        "+cons",
+        "-son",
+        "-cont",
+        "-nas",
+        "-strident",
+        "-lat",
+        "-voice",
+        "+spread",
+        "dorsal",
+        "+high",
+        "+back",
+      ],
+    },
+
+    {
+      name: "b",
+      tags: [
+        "+cons",
+        "-son",
+        "-cont",
+        "-nas",
+        "-strident",
+        "-lat",
+        "+voice",
+        "-spread",
+        "labial",
+      ],
+    },
+
+    // {
+    //   name: "",
+    //   tags: [
+    //     "cons",
+    //     "son",
+    //     "cont",
+    //     "nas",
+    //     "strident",
+    //     "lat",
+    //     "voice",
+    //     "spread",
+    //     "labial",
+    //     "coronal",
+    //     "ant",
+    //     "distr",
+    //     "dorsal",
+    //     "high",
+    //     "back",
+    //   ],
+    // },
+
+    // {
+    //   name: "",
+    //   tags: [
+    //     "cons",
+    //     "son",
+    //     "cont",
+    //     "nas",
+    //     "strident",
+    //     "lat",
+    //     "voice",
+    //     "spread",
+    //     "labial",
+    //     "coronal",
+    //     "ant",
+    //     "distr",
+    //     "dorsal",
+    //     "high",
+    //     "back",
+    //   ],
+    // },
+
+  ];
+
+
 });
