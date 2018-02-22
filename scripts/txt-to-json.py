e22ab2e1616b70e73afe74d4f67631a6d52a972a
Nafeu Nasir <nafeu.nasir@gmail.com>
2018-02-22 02:39:51 -0500

    Add dictionary parsing script

---
+++ scripts/txt-to-json.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+
+'''
+Author: Nafeu Nasir (github.com/nafeu)
+'''
+
+import sys, json
+
+export = {"content": []}
+with open('../data/lenape-english-dictionary.txt', 'r') as fp:
+    for line in fp:
+        out = line.split(" ", 1)
+        if len(out) > 1:
+            out[1] = out[1].rstrip("\n")
+            export["content"].append({
+              "name": out[0].decode('utf8'),
+              "def": out[1].decode('utf8')
+            })
+with open('../data/dict-export.json', 'w') as fp:
+    json.dump(export, fp)
